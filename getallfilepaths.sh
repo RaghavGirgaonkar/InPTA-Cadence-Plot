a3a235977ae81389d01f6b5c516347c5ede2105e
Raghav Girgaonkar <raghav.girgaonkar@gmail.com>
2022-02-28 22:58:08 +0530

    Update getallfilepaths.sh

--- getallfilepaths.sh
+++ getallfilepaths.sh
@@ -3,6 +3,6 @@ file="./AllPSRs.txt"
 while IFS= read line
 do
         # display $line or do something with $line
-        grep -r --include=\*.cmd.txt "PSR.*$line" ./ >> AllPSRSAllpaths.txt
+        grep -r --include=\*.cmd.txt --exclude=35_016_set1_3Feb2019.cmd.txt "PSR.*$line" ./ >> AllPSRSAllpaths.txt
 	echo "$line"
 done <"$file"
